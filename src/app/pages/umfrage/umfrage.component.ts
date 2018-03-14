e9723258022e364c3f918daf976badcddc51618c
sezarmbar <sezar_mbar@yahoo.com>
2018-03-14 12:46:42 +0100

    first commit

---
+++ src/app/pages/umfrage/umfrage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-umfrage',
+  templateUrl: './umfrage.component.html',
+  styleUrls: ['./umfrage.component.scss']
+})
+export class UmfrageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
