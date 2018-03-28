94f129ad70c51f0a15cfee5626ade33052c29275
sezarmbar <sezar_mbar@yahoo.com>
2018-03-28 14:13:55 +0200

    add mat-accordion

--- src/app/pages/umfrage-builder/umfrage-builder.component.ts
+++ src/app/pages/umfrage-builder/umfrage-builder.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./umfrage-builder.component.scss']
 })
 export class UmfrageBuilderComponent implements OnInit {
-
+  panelOpenState: boolean = false;
   constructor() { }
 
   ngOnInit() {
